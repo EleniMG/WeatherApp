cd5c3d725687cc3e9e108a624a6d7fd1256d76c6
Eleni <eleni@pop-os.localdomain>
2023-06-27 18:11:17 +0100

    Creating function to call the API with the user input

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let currentTemperature = 0
+let currentTemperature = ""
 
 async function getWeatherData(city) {
     await fetch(`https://api.weatherapi.com/v1/current.json?key=59d6cfb69d934532a51161240232606&q=${city}`, {mode: 'cors'}).then(response => {
@@ -7,8 +7,23 @@ async function getWeatherData(city) {
 
 }
 
-const locationToSearch = document.getElementById(location)
-console.log(locationToSearch)
+document.getElementById('submit').addEventListener('click', locationToSearch)
+
+function locationToSearch(event){
+    event.preventDefault();
+    const value = document.getElementById('location').value
+    console.log(getWeatherData(value))
+   
+}
+
+document.getElementById("search-results").innerHTML = currentTemperature
+
+// const submitButton = document.getElementById('submit');
+// submitButton.addEventListener('click', () => {
+//     let locationToSearch = document.getElementById("location")
+//     console.log(document.getElementById("location"))
+
+// })
 
 
-console.log(getWeatherData('london'))
+// console.log(getWeatherData('london'))
