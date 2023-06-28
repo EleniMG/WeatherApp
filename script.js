48e296e700b7de1931a739231e2b9c24240547b3
Eleni <eleni@pop-os.localdomain>
2023-06-28 16:22:15 +0100

    Displaying temperature on screen

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-let currentTemperature = ""
-
 async function getWeatherData(city) {
-    await fetch(`https://api.weatherapi.com/v1/current.json?key=59d6cfb69d934532a51161240232606&q=${city}`, {mode: 'cors'}).then(response => {
-        response.json().then(result => console.log(result.current.temp_c))
-    })
+    const response =  await fetch(`https://api.weatherapi.com/v1/current.json?key=59d6cfb69d934532a51161240232606&q=${city}`, {mode: 'cors'})
+    const jsonData = response.json()
+    const tempC = await jsonData.then(res => res.current.temp_c)
 
+    return tempC
 }
 
-document.getElementById('submit').addEventListener('click', locationToSearch)
-
-function locationToSearch(event){
+async function locationToSearch(event){
     event.preventDefault();
     const value = document.getElementById('location').value
-    console.log(getWeatherData(value))
-   
+    const currentTemperature = await getWeatherData(value)
+
+    document.getElementById("search-results").innerHTML = "The current temperature is " + currentTemperature +"°C"
 }
 
-document.getElementById("search-results").innerHTML = currentTemperature
 
-// const submitButton = document.getElementById('submit');
-// submitButton.addEventListener('click', () => {
-//     let locationToSearch = document.getElementById("location")
-//     console.log(document.getElementById("location"))
-
-// })
-
-
-// console.log(getWeatherData('london'))
+document.getElementById('submit').addEventListener('click', locationToSearch)
