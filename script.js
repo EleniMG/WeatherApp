33fa1d85b5d5d64ecdc12ead530c589d5da7912b
Eleni <eleni@pop-os.localdomain>
2023-06-28 14:24:48 +0100

    Altering how API is called to only obtain current temp in celsius

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let currentTemperature = ""
 
 async function getWeatherData(city) {
     await fetch(`https://api.weatherapi.com/v1/current.json?key=59d6cfb69d934532a51161240232606&q=${city}`, {mode: 'cors'}).then(response => {
-        response.json().then(result => console.log(result))
+        response.json().then(result => console.log(result.current.temp_c))
     })
 
 }
