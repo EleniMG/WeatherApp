e48bfa91bff70845add8cacb02dbef63826202de
Eleni <eleni@pop-os.localdomain>
2023-06-29 11:52:39 +0100

    Including Fahrenheit and creating an object for temperatures

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ async function getWeatherData(city) {
     const response =  await fetch(`https://api.weatherapi.com/v1/current.json?key=59d6cfb69d934532a51161240232606&q=${city}`, {mode: 'cors'})
     const jsonData = response.json()
     const tempC = await jsonData.then(res => res.current.temp_c)
-
-    return tempC
+    const tempF = await jsonData.then(res => res.current.temp_f)
+    return {tempC, tempF}
 }
 
 async function locationToSearch(event){
     event.preventDefault();
     const value = document.getElementById('location').value
-    const currentTemperature = await getWeatherData(value)
-
-    document.getElementById("search-results").innerHTML = "The current temperature is " + currentTemperature +"°C"
+    const currentTemperatureC = (await getWeatherData(value)).tempC
+    const currentTemperatureF = (await getWeatherData(value)).tempF
+    document.getElementById("search-results").innerHTML = "The current temperature is " + currentTemperatureC +"°C" + " and for my North American friends, " + currentTemperatureF + "°F." 
 }
 
 
 document.getElementById('submit').addEventListener('click', locationToSearch)
+
