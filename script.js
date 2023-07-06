af9c30cb02f5c3f72f1547b9592f68edf6de3d7d
Eleni <eleni@pop-os.localdomain>
2023-07-06 16:57:24 +0100

    Editing function to include weather icon

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function getWeatherData(city) {
     const jsonData = response.json()
     const tempC = await jsonData.then(res => res.current.temp_c)
     const tempF = await jsonData.then(res => res.current.temp_f)
-    const weatherCondition = await jsonData.then(res => res.current.condition.text)
+    const weatherCondition = await jsonData.then(res => res.current.condition)
     const locationName = await jsonData.then(res => res.location.name)
     const locationCountry = await jsonData.then(res => res.location.country)
     return {tempC, tempF, weatherCondition, locationName, locationCountry}
@@ -14,19 +14,21 @@ async function locationToSearch(event){
     const value = document.getElementById('location').value
     const currentTemperatureC = (await getWeatherData(value)).tempC
     const currentTemperatureF = (await getWeatherData(value)).tempF
-
-    const weatherConditionToDisplay = (await getWeatherData(value)).weatherCondition.toLowerCase()
+    const weatherConditionToDisplay = (await getWeatherData(value)).weatherCondition.text.toLowerCase()
 
     const areaName = (await getWeatherData(value)).locationName
     const countryName = (await getWeatherData(value)).locationCountry
 
     document.getElementById("search-results").innerHTML = "The current temperature is " + currentTemperatureC + "°C " + "and for my North American friends, " + currentTemperatureF + "°F in " + areaName + ", " + countryName + ". " + "The weather is currently " + weatherConditionToDisplay + ". "
+    const acceptableWeatherConditions = ['cloudy', 'fog', 'hail', 'rain', 'sunny', 'snow', 'thunder', 'partly cloudy']
+    const isAcceptableWeatherCondition = acceptableWeatherConditions.includes(weatherConditionToDisplay)
+    const urlPath = isAcceptableWeatherCondition ? `./${weatherConditionToDisplay.split(" ").join("-")}.svg` : './earth.svg'
     
-    // if (weatherCondition === "Sunny")
-
+    const img = document.createElement('img')
+    img.setAttribute("src", urlPath)
+    const iconResult = document.getElementById("icon-result")
+    document.getElementById("icon-result").replaceChildren()
+    document.getElementById('icon-result').appendChild(img)
 }
 
-
-
 document.getElementById('submit').addEventListener('click', locationToSearch)
-
